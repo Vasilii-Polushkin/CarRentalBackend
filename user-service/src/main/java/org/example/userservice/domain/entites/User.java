ed56f68a8a44c2911cb05061ebc3c82d5eac930c
Vasilii Polushkin <kvvarta@yandex.ru>
2025-03-29 03:22:28 +0700

    Initial commit

---
+++ user-service/src/main/java/org/example/userservice/domain/entites/User.java
@@ -0,0 +1,13 @@
+package org.example.userservice.domain.entites;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+}
